9214089d057d34ce86c225b89f0272641b11b772
Mark Columbus <mcolumbusua21@gmail.com>
2021-04-26 15:56:09 -0700

    fixes

--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -4,30 +4,34 @@ import "./style.css";
 function EmployeeCard(props) {
   return (
     <table>
-        <thead>
-          <tr>
-            <th scope="col">image</th>
-            <th scope="col">name</th>
-            <th scope="col">phone</th>
-            <th scope="col">email</th>
-            <th scope="col">DOB</th>
-            
+      <thead>
+        <tr>
+          <th scope="col">image</th>
+          <th scope="col">name</th>
+          <th scope="col">phone</th>
+          <th scope="col">email</th>
+          <th scope="col">DOB</th>
         </tr>
-        </thead>
-        <tbody>
-          {employees.map(employee => {
-            return (
-              <tr>
-                <td><img src={employee.picture.medium}/></td>
-                <td>{employee.name.first}{employee.name.last}</td>
-                <td>{employee.phone}</td>
-                <td>{employee.email}</td>
-                <td>{employee.DOB}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-        </table>
+      </thead>
+      <tbody>
+        {props.map((employee) => {
+          return (
+            <tr>
+              <td>
+                <img src={employee.picture.medium} />
+              </td>
+              <td>
+                {employee.name.first}
+                {employee.name.last}
+              </td>
+              <td>{employee.phone}</td>
+              <td>{employee.email}</td>
+              <td>{employee.DOB.date.slice(0,10)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
 
