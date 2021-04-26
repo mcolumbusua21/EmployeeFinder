4274f6c7f2872193d744cb9a9112efcdf74142d9
Mark Columbus <mcolumbusua21@gmail.com>
2021-04-25 18:07:57 -0700

    app js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
-import Tables from "./components/Tables";
 import API from "./utils/API";
+import Navbar from "./components/Navbar"
 
 class App extends React.Component {
   state = {
-    employees: [],
+    employee: [],
     searched: []
   };
 
-  componentDidMount() {
-    this.getEmployees();
-  }
-
-  getEmployees = async () => {
-    const { data } = await API.getUsers();
-    this.setState({ employees: data.results });
+   async componentDidMount() {
+     const { empData } = await API.getUsers();
+      this.setState({ employee: empData.results, searched: empData.results });
   };
+
 
   filterEmployees(employee){
     if (employee.name.first.includes(this.state.search)) return true;
@@ -24,10 +21,23 @@ class App extends React.Component {
     if (employee.email.includes(this.state.search)) return true;
   }
 
-  render() {
-   const { employees } = this.state;
-    return (
+  handleSort = () => {
+    const sortedArr = this.state.searched.sort((a, b) => a.name.first > b.name.first ? 1 : -1);
+    this.setState({searched: sortedArr});
+  };
   
+  handleInputChange = e => {
+    const value = e.target.value.toLowerCase();
+    const searchedArr = this.state.employees.filter(employee => employee.name.first.toLowerCase().startsWith(value))
+    this.setState({
+      searched: searchedArr,
+    });
+  };
+  
+
+  render() {
+    return (
+      { Navbar }
     )
   }
 }
